0d2a1e536f988968d5ad7da2a87f89a856e7362c
Ronapj1991 <51872051+Ronapj1991@users.noreply.github.com>
2021-11-01 13:40:29 -0400

    Create Part02_24.FromParameterToOne.java

---
+++ Part02_24.FromParameterToOne.java
@@ -0,0 +1,29 @@
+public class FromParameterToOne {
+
+ 
+
+    public static void main(String[] args) {
+
+ 
+
+ 
+
+    }
+
+ 
+
+    public static void printFromNumberToOne(int number) {
+
+        int count = 0;
+
+ 
+
+        while(number > count) {
+
+            System.out.println(number);
+
+            number--;
+
+        }
+
+    }
